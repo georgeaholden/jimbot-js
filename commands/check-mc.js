43745ada29435b50dc37f16be8e381c7c9df1d0f
George Holden <georgeaholden@gmail.com>
2022-03-14 23:59:35 +1300

    added emojis to /mc and fixed grammar for 1 player

--- commands/check-mc.js
+++ commands/check-mc.js
@@ -8,18 +8,26 @@ module.exports = {
     async execute(interaction) {
         serverStatus = await mcStatus.queryFull('mc.slimjims.life');
         response = formattedResponse(serverStatus);
-        await interaction.reply(response);
+        const message = await interaction.reply({content: response, fetchReply: true});
+        if (serverStatus.players.online == 0) {
+            message.react('<:Smodge:893005673888096286>')
+        } else {
+            message.react('<:HyperS:698500153342033930>')
+        }
     },
 }
 
 const formattedResponse = function(serverStatus) {
     result = 'The mc Server lives!\n';
     numPlayers = serverStatus.players.online;
-    if (numPlayers > 0) {
+    if (numPlayers > 1) {
         result += `There are ${numPlayers} players currently online:\n`;
         for (player of serverStatus.players.list) {
             result += player + '\n'
         }
+    } else if (numPlayers == 1) {
+        result += 'There is 1 player currently online:\n'
+        result += player + '\n'
     } else {
         result += "Nobody is currently online :'(";
     }
