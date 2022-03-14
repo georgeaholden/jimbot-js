fe1898170d89a318b0b899cf6d4ec5a0a842e564
George Holden <georgeaholden@gmail.com>
2022-03-14 17:27:26 +1300

    fixed upper case in isThanks

--- utils/string-matching.js
+++ utils/string-matching.js
@@ -1,6 +1,11 @@
-// This hard coding is gross but fast : D
-// I think this problem is always O(n^2)
+/**
+ * This hard coding is gross but fast to develop :D
+ * I think this problem is always O(n^2)?
+ * @param {string} message 
+ * @returns {bool}
+ */
 exports.isThanks = function(message) {
+    message = message.toLowerCase();
     let foundThank = false;
     for (thank of ['ty', 'thank', 'arigatou', 'gracias', 'cheers', 'chur', 'merci', 'onya', 'grazie', 'ta', 
     'shot', 'danke', 'appreciate', 'thx', 'tanks', 'gamsahabnida', 'Takk', 'Mahalo', 'Toda']){
